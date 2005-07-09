e107fc9e40bd6fbf0e6b96c9f2377dc3b58f30b0
jteigen <jteigen>
2005-07-09 03:33:04 +0000

    added method for returning streams as a ConsoleBufferVO object. removed unused methods

--- jacman/src/andyr/jacman/console/Run.java
+++ jacman/src/andyr/jacman/console/Run.java
@@ -1,78 +1,35 @@
 package andyr.jacman.console;
 
-
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class Run {
 
-	public Run() {
-	}
+	public static ConsoleBufferVO getConsoleBufferVO(String[] cmd,
+			String[] env, File dir) {
 
-	public static String[] it(String s) {
-                String returnVal[] = {"",""};
-		try {
-			Process p = Runtime.getRuntime().exec(s);
-			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String s2 = "";
-			while ((s2=br.readLine())!=null) {
-				//System.out.println(s2);
-                                returnVal[0] += (s2+"\n");
-			}
-			BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-			s2 = "";
-			while ((s2=bre.readLine())!=null) {
-				//System.out.println(s2);
-                                returnVal[1] += (s2+"\n");
-			}
-		} catch(Exception e) {
-                        //returnVal = e.getMessage();
-			System.out.println("Executable not found.");
-			e.printStackTrace();
-		}
-                return returnVal;
-	}
-        
-	public static String[] it(String[] cmd, String[] env, File dir) {
-                String returnVal[] = {"",""};
+		ConsoleBufferVO consoleBufferVO = new ConsoleBufferVO();
+
 		try {
 			Process p = Runtime.getRuntime().exec(cmd, env, dir);
-			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			String s2 = "";
-			while ((s2=br.readLine())!=null) {
-				//System.out.println(s2);
-                                returnVal[0] += (s2+"\n");
-			}
-			BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-			s2 = "";
-			while ((s2=bre.readLine())!=null) {
-				//System.out.println(s2);
-                                returnVal[1] += (s2+"\n");
-			}
-		} catch(Exception e) {
-                        //returnVal = e.getMessage();
-			System.out.println("Executable not found.");
-			e.printStackTrace();
+			BufferedReader bufferedReader = new BufferedReader(
+					new InputStreamReader(p.getInputStream()));
+			BufferedReader bufferedErrorReader = new BufferedReader(
+					new InputStreamReader(p.getErrorStream()));
+			BufferedWriter bufferedWriter = new BufferedWriter(
+					new OutputStreamWriter(p.getOutputStream()));
+
+			consoleBufferVO.setBufferedReader(bufferedReader);
+			consoleBufferVO.setBufferedErrorReader(bufferedErrorReader);
+			consoleBufferVO.setBufferedWriter(bufferedWriter);
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
 		}
-                return returnVal;
-	}
-        
-        public static BufferedReader[] it(String[] cmd, String[] env, File dir, boolean useBufferedOutputs) {
-                BufferedReader returnVal[] = {null,null};
-		try {
-			Process p = Runtime.getRuntime().exec(cmd, env, dir);
-			BufferedReader br = new BufferedReader(new InputStreamReader(p.getInputStream()));
-			BufferedReader bre = new BufferedReader(new InputStreamReader(p.getErrorStream()));
-                        returnVal[0]=br;
-                        returnVal[1]=bre;
-		} catch(Exception e) {
-                        //returnVal = e.getMessage();
-			System.err.println("Executable not found.");
-			e.printStackTrace();
-		}
-                return returnVal;
+		return consoleBufferVO;
 	}
 
-	
 }
