0eb347bf7eb70b3e51160a4ed9924e2ffc932ee9
jteigen <jteigen>
2005-07-19 14:21:14 +0000

    removed unneeded import statement

--- jacman/src/andyr/jacman/Jacman.java
+++ jacman/src/andyr/jacman/Jacman.java
@@ -18,7 +18,6 @@ import java.io.IOException;
 import java.net.URLClassLoader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 
