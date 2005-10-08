1b86fc903fabab1c0e257ae0f7f5df53c35df8c4
arooaroo <arooaroo>
2005-10-08 17:00:32 +0000

    Added license and tidied code

--- jacman/src/andyr/jacman/InstallListFilter.java
+++ jacman/src/andyr/jacman/InstallListFilter.java
@@ -1,34 +1,47 @@
 /*
- * Created on Jun 21, 2005
- *
+ * 
+ * Copyright 2005 The Jacman Team
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
  */
+
 package andyr.jacman;
-
-
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 import javax.swing.JList;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
 
+import andyr.jacman.utils.I18nManager;
 import ca.odell.glazedlists.AbstractFilterList;
 import ca.odell.glazedlists.BasicEventList;
-import ca.odell.glazedlists.CollectionList;
 import ca.odell.glazedlists.EventList;
-import ca.odell.glazedlists.UniqueList;
 import ca.odell.glazedlists.swing.EventListModel;
 import ca.odell.glazedlists.swing.EventSelectionModel;
 
 public class InstallListFilter extends AbstractFilterList implements ListSelectionListener {
 
     /** a list of users */
-    private EventList usersEventList;
-    private EventList usersSelectedList;
+    private EventList packagesEventList;
+    private EventList packagesSelectedList;
 
     /** a widget for selecting users */
     private JList userSelect;
+    
+    private I18nManager i18n;
 
     /**
      * Create a filter list that filters the specified source list, which
@@ -37,22 +50,23 @@ public class InstallListFilter extends AbstractFilterList implements ListSelecti
     public InstallListFilter(EventList source) {
         super(source);
 
+        i18n = I18nManager.getI18nManager("i18n/JacmanLabels", Locale.getDefault());
         
         List filterItems = new ArrayList();
-        filterItems.add("All");
-        filterItems.add("Not installed");
+        filterItems.add(i18n.getString("FilterAll"));
+        filterItems.add(i18n.getString("FilterNotInstalled"));
         
         // create a unique users list from the source issues list
         //usersEventList = new UniqueList(new CollectionList(source, new IssueUserator()));
-        usersEventList = new BasicEventList(filterItems);
+        packagesEventList = new BasicEventList(filterItems);
         // create a JList that contains users
-        EventListModel usersListModel = new EventListModel(usersEventList);
+        EventListModel usersListModel = new EventListModel(packagesEventList);
         userSelect = new JList(usersListModel);
 
         // create an EventList containing the JList's selection
-        EventSelectionModel userSelectionModel = new EventSelectionModel(usersEventList);
+        EventSelectionModel userSelectionModel = new EventSelectionModel(packagesEventList);
         userSelect.setSelectionModel(userSelectionModel);
-        usersSelectedList = userSelectionModel.getSelected();
+        packagesSelectedList = userSelectionModel.getSelected();
         userSelect.addListSelectionListener(this);
 
         handleFilterChanged();
@@ -79,30 +93,20 @@ public class InstallListFilter extends AbstractFilterList implements ListSelecti
      */
     public boolean filterMatches(Object o) {
         if (o == null) return false;
-        if (usersSelectedList.isEmpty()) return true;
+        if (packagesSelectedList.isEmpty()) return true;
         PacmanPkg pkg = (PacmanPkg)o;
         
-        if (usersSelectedList.contains("All")) {
+        if (packagesSelectedList.contains("All")) {
             return true;
         }
-        else if (usersSelectedList.contains("Not installed")) {
+        else if (packagesSelectedList.contains("Not installed")) {
             if (pkg.getInstalledVersion().equals("--")) {
-                //System.out.println(pkg.getName() + " not installed");
                 return true;
             }
         }
         
         return false;
         
-        // see if the two lists have just one intersection
-        /*List users = issue.getAllUsers();
-        for(Iterator u = users.iterator(); u.hasNext(); ) {
-            String user = (String)u.next();
-            if(usersSelectedList.contains(user)) return true;
-        }*/
-
-        // no intersection
-        //return false;
     }
 }
 
