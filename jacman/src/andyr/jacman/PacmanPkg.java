898d3800ed39d3a26dda681b4cb0143b7ed1233e
jteigen <jteigen>
2005-10-12 21:00:53 +0000

    @override

--- jacman/src/andyr/jacman/PacmanPkg.java
+++ jacman/src/andyr/jacman/PacmanPkg.java
@@ -7,234 +7,225 @@ import java.io.*;
 
 public class PacmanPkg implements Comparable<PacmanPkg> {
 
-    protected String name = "";
-    protected String version = "";
-    protected String installedVersion = "";
-    protected String description = "";
-    protected String repository = "";
-    protected String md5sum = "";
-    protected long size = -1l;
-    protected List<String> depends = new ArrayList<String>();
-    protected boolean installSelected = false;
+	protected String name = "";
+	protected String version = "";
+	protected String installedVersion = "";
+	protected String description = "";
+	protected String repository = "";
+	protected String md5sum = "";
+	protected long size = -1l;
+	protected List<String> depends = new ArrayList<String>();
+	protected boolean installSelected = false;
 
-    public PacmanPkg() {
+	public PacmanPkg() {
 
-    }
+	}
 
-    public PacmanPkg(File packagePath) throws IOException {
-        obtainPackageInfo(packagePath);
-        
-        setInstalledVersion("--");
-    }
+	public PacmanPkg(File packagePath) throws IOException {
+		obtainPackageInfo(packagePath);
 
-    protected void processInfoBuffer(String infoBuffer) {
+		setInstalledVersion("--");
+	}
 
-        int pos = infoBuffer.lastIndexOf('%');
+	protected void processInfoBuffer(String infoBuffer) {
 
-        if (pos != -1) {
-            String header = infoBuffer.substring(0, pos+1);
-            String info = infoBuffer.substring(pos+1);
+		int pos = infoBuffer.lastIndexOf('%');
 
-            if (header.startsWith("%NAME%")) {
-                this.setName(info);
-            }
-            else if (header.startsWith("%DESC%")) {
-                this.setDescription(info);
-            }
-            else if (header.startsWith("%VERSION%")) {
-                this.setVersion(info);
-            }
-            else if (header.startsWith("%CSIZE%")) {
-                this.setSize(Long.parseLong(info));
-            }
-            else if (header.startsWith("%MD5SUM%")) {
-                this.setMd5sum(info);
-            }
-        }
+		if (pos != -1) {
+			String header = infoBuffer.substring(0, pos + 1);
+			String info = infoBuffer.substring(pos + 1);
 
-  }
-    protected void processDependsBuffer(String depBuffer) {
-        int pos = depBuffer.lastIndexOf('%');
+			if (header.startsWith("%NAME%")) {
+				this.setName(info);
+			} else if (header.startsWith("%DESC%")) {
+				this.setDescription(info);
+			} else if (header.startsWith("%VERSION%")) {
+				this.setVersion(info);
+			} else if (header.startsWith("%CSIZE%")) {
+				this.setSize(Long.parseLong(info));
+			} else if (header.startsWith("%MD5SUM%")) {
+				this.setMd5sum(info);
+			}
+		}
 
-        if (pos != -1) {
-            String header = depBuffer.substring(0, pos+1);
-            String info = depBuffer.substring(pos+1);
+	}
 
-            if (header.startsWith("%DEPENDS%")) {
-                StringTokenizer tokens = new StringTokenizer(info);
+	protected void processDependsBuffer(String depBuffer) {
+		int pos = depBuffer.lastIndexOf('%');
 
-                while (tokens.hasMoreTokens()) {
-                    this.addDepends(tokens.nextToken());
-                }
-            }
+		if (pos != -1) {
+			String header = depBuffer.substring(0, pos + 1);
+			String info = depBuffer.substring(pos + 1);
 
-        }
-    }
+			if (header.startsWith("%DEPENDS%")) {
+				StringTokenizer tokens = new StringTokenizer(info);
 
-    public void obtainPackageInfo(File packagePath) throws IOException {
+				while (tokens.hasMoreTokens()) {
+					this.addDepends(tokens.nextToken());
+				}
+			}
 
-        // The packagePath is a dir containing details about a package in that repo.
+		}
+	}
 
-        // Parse the 'desc' file contain information about the package.
-        File descPath = new File(packagePath.getPath() + "/desc");
+	public void obtainPackageInfo(File packagePath) throws IOException {
 
-        BufferedReader reader = new BufferedReader(new FileReader(descPath));
-        String buffer = "";
-        String infoBuffer = "";
+		// The packagePath is a dir containing details about a package in that
+		// repo.
 
-        while ((buffer = reader.readLine()) != null) {
-            buffer = buffer.trim();
+		// Parse the 'desc' file contain information about the package.
+		File descPath = new File(packagePath.getPath() + "/desc");
 
-            if (buffer.startsWith("%")) {
-                processInfoBuffer(infoBuffer);
-                infoBuffer = buffer;
-            }
-            else if (!buffer.equals("")) {
-                infoBuffer += buffer;
-            }
+		BufferedReader reader = new BufferedReader(new FileReader(descPath));
+		String buffer = "";
+		String infoBuffer = "";
 
-        }
-        processInfoBuffer(infoBuffer);
-        reader.close();
+		while ((buffer = reader.readLine()) != null) {
+			buffer = buffer.trim();
 
-        // Parse the 'depends' file to obtain the packages that this package
-        // depends on.
+			if (buffer.startsWith("%")) {
+				processInfoBuffer(infoBuffer);
+				infoBuffer = buffer;
+			} else if (!buffer.equals("")) {
+				infoBuffer += buffer;
+			}
 
-        File dependsPath = new File(packagePath.getPath() + "/depends");
+		}
+		processInfoBuffer(infoBuffer);
+		reader.close();
 
-        reader = new BufferedReader(new FileReader(dependsPath));
-        buffer = "";
-        infoBuffer = "";
+		// Parse the 'depends' file to obtain the packages that this package
+		// depends on.
 
-        while ((buffer = reader.readLine()) != null) {
-            buffer = buffer.trim();
+		File dependsPath = new File(packagePath.getPath() + "/depends");
 
-            if (buffer.startsWith("%")) {
-                processDependsBuffer(infoBuffer);
-                infoBuffer = buffer;
-            }
-            else if (!buffer.equals("")) {
-                infoBuffer += " " + buffer;
-            }
+		reader = new BufferedReader(new FileReader(dependsPath));
+		buffer = "";
+		infoBuffer = "";
 
+		while ((buffer = reader.readLine()) != null) {
+			buffer = buffer.trim();
 
-        }
-        processDependsBuffer(infoBuffer);
-        reader.close();
+			if (buffer.startsWith("%")) {
+				processDependsBuffer(infoBuffer);
+				infoBuffer = buffer;
+			} else if (!buffer.equals("")) {
+				infoBuffer += " " + buffer;
+			}
 
-        // The repository can be obtained from the path itself.
+		}
+		processDependsBuffer(infoBuffer);
+		reader.close();
 
-        setRepository(packagePath.getParentFile().getName());
+		// The repository can be obtained from the path itself.
 
-    }
+		setRepository(packagePath.getParentFile().getName());
 
-    public String getName() {
-        return name;
-    }
+	}
 
-    public void setName(String name) {
-        this.name = name;
-    }
+	public String getName() {
+		return name;
+	}
 
-    public String getVersion() {
-        return version;
-    }
+	public void setName(String name) {
+		this.name = name;
+	}
 
-    public void setVersion(String version) {
-        this.version = version;
-    }
-    
-    public String getInstalledVersion() {
-        return installedVersion;
-    }
+	public String getVersion() {
+		return version;
+	}
 
-    public void setInstalledVersion(String installedVersion) {
-        this.installedVersion = installedVersion;
-    }
+	public void setVersion(String version) {
+		this.version = version;
+	}
 
-    public String getMd5sum() {
-        return md5sum;
-    }
+	public String getInstalledVersion() {
+		return installedVersion;
+	}
 
-    public void setMd5sum(String md5sum) {
-        this.md5sum = md5sum;
-    }
+	public void setInstalledVersion(String installedVersion) {
+		this.installedVersion = installedVersion;
+	}
 
-    public String getDescription() {
-        return description;
-    }
+	public String getMd5sum() {
+		return md5sum;
+	}
 
-    public void setDescription(String description) {
-        this.description = description;
-    }
+	public void setMd5sum(String md5sum) {
+		this.md5sum = md5sum;
+	}
 
-    public long getSize() {
-        return size;
-    }
+	public String getDescription() {
+		return description;
+	}
 
-    public void setSize(long size) {
-        this.size = size;
-    }
-    
-    public boolean getInstallSelected() {
-        return installSelected;
-    }
-    
-    public void setInstallSelected(boolean selected) {
-        this.installSelected = selected;
-    }
+	public void setDescription(String description) {
+		this.description = description;
+	}
 
-    public String getRepository() {
-        return repository;
-    }
+	public long getSize() {
+		return size;
+	}
 
-    public void setRepository(String repository) {
-        this.repository = repository;
-    }
+	public void setSize(long size) {
+		this.size = size;
+	}
 
-    public List<String> getDepends() {
-        return depends;
-    }
+	public boolean getInstallSelected() {
+		return installSelected;
+	}
 
-    public void setDepends(List<String> depends) {
-        this.depends = depends;
-    }
+	public void setInstallSelected(boolean selected) {
+		this.installSelected = selected;
+	}
 
-    public void addDepends(String newDepend) {
+	public String getRepository() {
+		return repository;
+	}
 
-        depends.add(newDepend);
-    }
+	public void setRepository(String repository) {
+		this.repository = repository;
+	}
 
-    public String toString() {
+	public List<String> getDepends() {
+		return depends;
+	}
 
-        StringBuilder output = new StringBuilder();
+	public void setDepends(List<String> depends) {
+		this.depends = depends;
+	}
 
-        output.append("Package name: " + getName() + '\n');
-        output.append("Package desc: " + getDescription() + '\n');
-        output.append("Package repository: " + getRepository() + '\n');
-        output.append("Package size: " + getSize() + '\n');
-        output.append("Package md5sum: " + getMd5sum() + '\n');
-        output.append("Package depends on: ");
+	public void addDepends(String newDepend) {
 
-        for (int i = 0; i < depends.size(); i++) {
-            output.append((String) depends.get(i) + " ");
+		depends.add(newDepend);
+	}
 
-        }
-        output.append('\n');
+	@Override
+	public String toString() {
 
-        return output.toString();
-    }
+		StringBuilder output = new StringBuilder();
 
-    public int compareTo(PacmanPkg p) {
+		output.append("Package name: " + getName() + '\n');
+		output.append("Package desc: " + getDescription() + '\n');
+		output.append("Package repository: " + getRepository() + '\n');
+		output.append("Package size: " + getSize() + '\n');
+		output.append("Package md5sum: " + getMd5sum() + '\n');
+		output.append("Package depends on: ");
 
+		for (int i = 0; i < depends.size(); i++) {
+			output.append(depends.get(i) + " ");
 
-        if (getName().equals(p.getName()) ) {
-            return getVersion().compareTo(p.getVersion());
-        }
-        else {
-            return getName().compareTo(p.getName());
-        }
-    }
+		}
+		output.append('\n');
 
+		return output.toString();
+	}
+
+	public int compareTo(PacmanPkg p) {
+
+		if (getName().equals(p.getName()))
+			return getVersion().compareTo(p.getVersion());
+		return getName().compareTo(p.getName());
+	}
 
 }
--- jacman/src/andyr/jacman/gui/ElegantPanel.java
+++ jacman/src/andyr/jacman/gui/ElegantPanel.java
@@ -1,6 +1,5 @@
 package andyr.jacman.gui;
 
-
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.GradientPaint;
@@ -12,95 +11,96 @@ import java.awt.LayoutManager;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-
-
-
-
 public class ElegantPanel extends JPanel {
 	protected static final Color GRAD_MID = new Color(168, 203, 239);
 	protected static final Color GRAD_START = new Color(10, 36, 106);
-	
+
 	protected final int TITLE_HEIGHT = 25;
-	
+
 	protected JLabel titleLabel;
 	protected JPanel mainPanel;
-	
+
 	public ElegantPanel(String title, JPanel content) {
 		super.add(getTitleLabel());
-		
+
 		mainPanel = content;
-		//contents.add(new JButton("Testing"));
+		// contents.add(new JButton("Testing"));
 		super.add(mainPanel);
-		
+
 		setTitle(title);
-        
-        setBorder(new ShadowBorder());
-        
-		//dockable = getDockable();
+
+		setBorder(new ShadowBorder());
+
+		// dockable = getDockable();
 	}
-	
+
+	@Override
 	public void doLayout() {
 		Insets insets = getInsets();
-		int w = getWidth()-insets.left-insets.right;
-		int h = getHeight()-insets.top-insets.bottom-6;
-		getTitleLabel().setBounds(insets.left+3, insets.top, w, TITLE_HEIGHT);
-		
-		mainPanel.setBounds(insets.left+3, insets.top + TITLE_HEIGHT+3, w-6, h - TITLE_HEIGHT);
-		
+		int w = getWidth() - insets.left - insets.right;
+		int h = getHeight() - insets.top - insets.bottom - 6;
+		getTitleLabel().setBounds(insets.left + 3, insets.top, w, TITLE_HEIGHT);
+
+		mainPanel.setBounds(insets.left + 3, insets.top + TITLE_HEIGHT + 3,
+				w - 6, h - TITLE_HEIGHT);
+
 	}
-	
-	/*public Dockable getDockable() {
-		if(dockable==null)
-			dockable = new DockableImpl(this, getTitleLabel());
-		return dockable;
-	}*/
-	
+
+	/*
+	 * public Dockable getDockable() { if(dockable==null) dockable = new
+	 * DockableImpl(this, getTitleLabel()); return dockable; }
+	 */
+
 	public String getTitle() {
 		return getTitleLabel().getText();
 	}
 
 	private JLabel getTitleLabel() {
-		if(titleLabel!=null)
+		if (titleLabel != null)
 			return titleLabel;
-			
+
 		titleLabel = new JLabel();
 		titleLabel.setForeground(Color.white);
 		titleLabel.setFont(titleLabel.getFont().deriveFont(Font.PLAIN));
 		return titleLabel;
 	}
-	
+
+	@Override
 	public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		Insets in = getInsets();
-		int mid = getWidth()/2;
-		int y = in.top + 13; //was 13
-		int farRight = getWidth()-in.right;
+		int mid = getWidth() / 2;
+		int y = in.top + 13; // was 13
+		int farRight = getWidth() - in.right;
 		int w = farRight - in.left;
-		
-		GradientPaint firstHalf = new GradientPaint(in.left, y, GRAD_START, mid, y, GRAD_MID);
-		GradientPaint secondHalf = new GradientPaint(mid, y, GRAD_MID, farRight, y, getBackground());
-		
-		Graphics2D g2 = (Graphics2D)g;
+
+		GradientPaint firstHalf = new GradientPaint(in.left, y, GRAD_START,
+				mid, y, GRAD_MID);
+		GradientPaint secondHalf = new GradientPaint(mid, y, GRAD_MID,
+				farRight, y, getBackground());
+
+		Graphics2D g2 = (Graphics2D) g;
 		g2.setPaint(firstHalf);
-		g2.fillRect(in.left, in.top, w/2, TITLE_HEIGHT);
+		g2.fillRect(in.left, in.top, w / 2, TITLE_HEIGHT);
 		g2.setPaint(secondHalf);
-		g2.fillRect(mid-1, in.top, w/2, TITLE_HEIGHT);
-		
+		g2.fillRect(mid - 1, in.top, w / 2, TITLE_HEIGHT);
+
 		g.setColor(getBackground().brighter());
-		g.drawLine(in.left, in.top, farRight,  in.top);
-		g.drawLine(in.left, in.top, in.left, in.top+TITLE_HEIGHT);
-		
+		g.drawLine(in.left, in.top, farRight, in.top);
+		g.drawLine(in.left, in.top, in.left, in.top + TITLE_HEIGHT);
+
 		g.setColor(getBackground().darker());
-		g.drawLine(in.left, in.top+TITLE_HEIGHT, farRight, in.top+TITLE_HEIGHT);
+		g.drawLine(in.left, in.top + TITLE_HEIGHT, farRight, in.top
+				+ TITLE_HEIGHT);
 	}
-	
+
 	public void setLayout(LayoutManager mgr) {
-		// do nothing.  we handle our own layout.
+		// do nothing. we handle our own layout.
 	}
 
 	public void setTitle(String title) {
-		if(title==null)
+		if (title == null)
 			title = "";
 		title = title.trim();
 		getTitleLabel().setText(title);
