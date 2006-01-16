7ed7fbbf76f03664aa21b90f7be836ebfa3a87bd
arooaroo <arooaroo>
2006-01-16 23:02:15 +0000

    Updated LocaleChanger: no live locale change - instead a message to inform user a restart is required. (Short term work-around).

--- jacman/src/andyr/jacman/gui/LocaleChanger.java
+++ jacman/src/andyr/jacman/gui/LocaleChanger.java
@@ -17,8 +17,8 @@
 package andyr.jacman.gui;
 
 import java.awt.ComponentOrientation;
-import java.awt.event.ItemEvent;
-import java.awt.event.ItemListener;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.URLClassLoader;
 import java.text.Collator;
 import java.text.RuleBasedCollator;
@@ -30,12 +30,12 @@ import javax.swing.ButtonGroup;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
+import javax.swing.JOptionPane;
 import javax.swing.JRadioButtonMenuItem;
 
-import andyr.jacman.Jacman;
 import andyr.jacman.utils.I18nManager;
 
-public class LocaleChanger extends JMenu implements ItemListener {
+public class LocaleChanger extends JMenu implements ActionListener {
     
     private I18nManager i18n;
     private ComponentOrientation co;
@@ -60,32 +60,32 @@ public class LocaleChanger extends JMenu implements ItemListener {
         // locale
         RuleBasedCollator rbc = (RuleBasedCollator) Collator.getInstance();
         ArrayList<String> al = new ArrayList<String>();
-        al.add(i18n.getString("LanguageChinese"));
+        //al.add(i18n.getString("LanguageChinese"));
         al.add(i18n.getString("LanguageEnglish"));
-        al.add(i18n.getString("LanguageFinnish"));
-        al.add(i18n.getString("LanguageFrench"));
-        al.add(i18n.getString("LanguageGreek"));
+        //al.add(i18n.getString("LanguageFinnish"));
+        //al.add(i18n.getString("LanguageFrench"));
+        //al.add(i18n.getString("LanguageGreek"));
         al.add(i18n.getString("LanguagePolish"));
-        al.add(i18n.getString("LanguageSpanish"));
-        al.add(i18n.getString("LanguageSwedish"));
+        //al.add(i18n.getString("LanguageSpanish"));
+        //al.add(i18n.getString("LanguageSwedish"));
 
         Collections.sort(al, rbc);
 
         String langName = Locale.getDefault().getDisplayLanguage();
+        
         for (int i = 0; i < al.size(); i++) {
             JRadioButtonMenuItem mi;
-            mi = (JRadioButtonMenuItem) add(new JRadioButtonMenuItem(
-                    (String) al.get(i)));
+            mi = (JRadioButtonMenuItem) add(new JRadioButtonMenuItem((String) al.get(i)));
 
             if (langName.equalsIgnoreCase((String) al.get(i))) {
                 mi.setSelected(true);
             }
-            mi.addItemListener(this);
+            mi.addActionListener(this);
             langGroup.add(mi);
         }
     }
 
-    public void itemStateChanged(ItemEvent e) {
+    public void actionPerformed(ActionEvent e) {
         try {
             JRadioButtonMenuItem rb = (JRadioButtonMenuItem) e.getSource();
             if (rb.isSelected()) {
@@ -95,12 +95,12 @@ public class LocaleChanger extends JMenu implements ItemListener {
                     co = ComponentOrientation.LEFT_TO_RIGHT;
                 } 
                 else if (selected.equals(i18n.getString("LanguageEnglish"))) {
-                    Locale.setDefault(Locale.UK);
+                    Locale.setDefault(Locale.US);
                     co = ComponentOrientation.LEFT_TO_RIGHT;
                 } 
                 else if (selected.equals(i18n.getString("LanguageFinnish"))) {
-                        Locale.setDefault(new Locale("fi"));
-                        co = ComponentOrientation.LEFT_TO_RIGHT;
+                    Locale.setDefault(new Locale("fi"));
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
                 }
                 else if (selected.equals(i18n.getString("LanguageFrench"))) {
                     Locale.setDefault(Locale.FRENCH);
@@ -131,21 +131,71 @@ public class LocaleChanger extends JMenu implements ItemListener {
             
             i18n.setLocale(Locale.getDefault());
             
-            if (co != null) {
-                Jacman jacman = new Jacman();
-                jacman.createGUI();
-                parent.getContentPane().removeAll();
-                parent.setJMenuBar(jacman.getJacmanMenus());
-                parent.setContentPane(jacman.getFrame().getContentPane());
-                parent.applyComponentOrientation(co);
-                parent.repaint();
-            }
-
+            JOptionPane.showMessageDialog(parent, i18n.getString("GeneralOptionsStartInTrayDesc"));
+            
         } 
         catch (Exception ex) {
             ex.printStackTrace(System.out);
         }
 
     }
+    
+    /*public void itemStateChanged(ItemEvent e) {
+        try {
+            JRadioButtonMenuItem rb = (JRadioButtonMenuItem) e.getSource();
+            if (rb.isSelected()) {
+                String selected = rb.getText();
+                if (selected.equals(i18n.getString("LanguageChinese"))) {
+                    Locale.setDefault(Locale.CHINESE);
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
+                } 
+                else if (selected.equals(i18n.getString("LanguageEnglish"))) {
+                    System.out.println("Selected English");
+                    Locale.setDefault(Locale.UK);
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
+                } 
+                else if (selected.equals(i18n.getString("LanguageFinnish"))) {
+                        Locale.setDefault(new Locale("fi"));
+                        co = ComponentOrientation.LEFT_TO_RIGHT;
+                }
+                else if (selected.equals(i18n.getString("LanguageFrench"))) {
+                    Locale.setDefault(Locale.FRENCH);
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
+                }
+                else if (selected.equals(i18n.getString("LanguageGreek"))) {
+                    Locale.setDefault(new Locale("el"));
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
+                }
+                else if (selected.equals(i18n.getString("LanguagePolish"))) {
+                    System.out.println("Selected Polish");
+                    Locale.setDefault(new Locale("pl"));
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
+                }
+                else if (selected.equals(i18n.getString("LanguageSpanish"))) {
+                    Locale.setDefault(new Locale("es"));
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
+                }
+                else if (selected.equals(i18n.getString("LanguageSwedish"))) {
+                    Locale.setDefault(new Locale("sv"));
+                    co = ComponentOrientation.LEFT_TO_RIGHT;
+                }
+            }
+            
+            
+            //System.out.println(Locale.getDefault());
+            i18n.setLocale(Locale.getDefault());
+            //parent.validate();
+            //System.out.println(Locale.getDefault());
+            
+            JOptionPane.showMessageDialog(parent, i18n.getString("GeneralOptionsStartInTrayDesc"));
+            
+                  
+            
+        } 
+        catch (Exception ex) {
+            ex.printStackTrace(System.out);
+        }
+
+    }*/
 }
 
